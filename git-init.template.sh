5272371a0ae82e73c46004459e6dd3fc3da030da
Elvin <dev@minimall.io>
2026-02-15 17:24:47 -0800

    Fix .gitignore to track the `.devcontainer` contents and explain the git-init.template.sh use.

--- git-init.template.sh
+++ git-init.template.sh
@@ -1,3 +1,5 @@
+# Clone this file and rename the clone to `git-init.sh`
+# if you want to use this template for configuring the global git.
 git config --global user.email "user.email@minimall.io"
 git config --global user.name "minimall-user.name"
 git config --global core.editor "code"
